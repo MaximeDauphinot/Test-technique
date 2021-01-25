371fbde25488fecd937d415261a8828806636900
Mdauphin42 <mdauphin@student.42.fr>
2021-01-25 15:45:51 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+var root = document.body;
+var count = 0;
+
+var increment = function () {
+  m.request({
+    method: "PUT",
+    url: "//rem-rest-api.herokuapp.com/api/tutorial/1",
+    body: { count: count + 1 },
+    withCredentials: true,
+  }).then(function (data) {
+    count = parseInt(data.count);
+  });
+};
+
+var Hello = {
+  view: function () {
+    return m("main", [
+      m(
+        "h1",
+        {
+          class: "title",
+        },
+        "My first app"
+      ),
+      m(
+        "button",
+        {
+          onclick: increment,
+        },
+        count + " clicks"
+      ),
+    ]);
+  },
+};
+
+m.mount(root, Hello);
